89cfdd8a325a2aab8c3d9aa87db6e4993cf24af8
vhifadit <daagbohounondekavhifadit@gmail.com>
2026-02-07 03:49:48 +0100

    mise en place d'un schemat de donnee pour l'authentification

--- models/user.js
+++ models/user.js
@@ -1,1 +1,19 @@
-//pour creer des doc utilisateur 
+//pour creer des doc utilisateurs
+
+const mongoose= require('mongoose')
+/*mongoose-unique-validator  améliore les
+ messages d'erreur lors de l'enregistrement 
+ de données uniques.*/
+const uniqueValidator=require('mongoose-unique-validator')
+
+
+const userSchema=mongoose.Schema({
+    email:{type:String,required:true,unique:true},
+    /*l'attribut unique a ete ajouter ici 
+    afin que ca soit qu'un utilsation ne 
+    s'inscrive pas   avec plusieurs email*/
+    password:{type:String,required:true}
+})
+userSchema.plugin(uniqueValidator)
+
+module.exports= mongoose.model('User',userSchema)
